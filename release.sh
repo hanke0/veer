c06447117b95168e2cc2f0d58c99a06e618a21c0
hanke <hanke0@outlook.com>
2024-02-20 12:45:07 +0800

    add action

---
+++ release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -Eeo pipefail
+
+release_platform() {
+    while [ $# -gt 0 ]; do
+        name="./dist/veer-$1-$2"
+        if [ "$1" = "windows" ]; then
+            name="$name.exe"
+        fi
+        GOOS=$1 GOARCH=$2 go build -o "$name" .
+        shift 2
+    done
+}
+
+rm -rf ./dist
+mkdir -p ./dist
+
+release_platform \
+    linux amd64 \
+    linux 386 \
+    windows amd64 \
+    windows 386 \
+    darwin arm64 \
+    darwin amd64
+
+cd ./dist
+md5sum >md5.sum ./*
